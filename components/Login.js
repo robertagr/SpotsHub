36bd76fbb8f8c75d92e0dcd5bd550312bf9bebaa
ROBERTA GRASSO <robertagrasso95@hotmail.com>
2023-07-27 19:35:52 +0200

    login with google added

--- components/Login.js
+++ components/Login.js
@@ -3,24 +3,17 @@ import { useSession, signIn, signOut } from "next-auth/react";
 export default function Login() {
   const { data: session } = useSession();
 
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.name} <br />
-        {/* <Image
-          src={session.user.image}
-          width={50}
-          height={50}
-          alt={session.user}
-        ></Image> */}
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
+  // if (session) {
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
+    <div>
+      {session ? (
+        <>
+          <p>Welcome, {session.user.name}!</p>
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      ) : (
+        <button onClick={() => signIn()}>Sign in with GitHub</button>
+      )}
+    </div>
   );
 }
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+// import Providers from "next-auth/providers";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -8,7 +10,10 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    // ...add more providers here
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
   ],
 };
 
