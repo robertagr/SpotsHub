dc4fb0a5bc6a65cf8f761d39938da16cefc2247b
ROBERTA GRASSO <robertagrasso95@hotmail.com>
2023-08-07 19:01:18 +0200

    profile page

--- components/LoginButton.js
+++ components/LoginButton.js
@@ -1,4 +1,48 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import styled from "styled-components";
+
+const WelcomeText = styled.div`
+  color: gray;
+  margin-left: 0px;
+  font-size: 15px;
+  font-weight: normal;
+  margin-right: 88px;
+
+  /* margin-left: 0px; */
+`;
+
+const UserName = styled.div`
+  color: gray;
+  margin-left: 0px;
+  font-size: 15px;
+  font-weight: normal;
+  font-weight: bold;
+  margin-right: 40px;
+`;
+
+const WelcomeContainer = styled.div`
+  /* display: flex; */
+  /* align-items: center; */
+  padding: 25px 10px 20px 6px;
+  /* margin-right: 100%; */
+`;
+
+const SignInButton = styled.button`
+  border-radius: 10px;
+  border-style: none;
+  width: 80px;
+  height: 30px;
+  /* margin: 0px 0px 5px 200px; */
+  margin-left: 10px;
+  cursor: pointer;
+  &:hover {
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
 export default function LoginButton() {
   const { data: session, error, isLoading } = useSession();
@@ -7,13 +51,18 @@ export default function LoginButton() {
     <div>
       {session ? (
         <>
-          <p>Welcome, {session.user.name}!</p>
-          <button onClick={() => signOut()}>Sign out</button>
+          <Wrapper>
+            <WelcomeContainer>
+              <WelcomeText>Welcome,</WelcomeText>
+              <UserName>{session.user.name}</UserName>
+            </WelcomeContainer>
+            <SignInButton onClick={() => signOut()}>Sign out</SignInButton>
+          </Wrapper>
         </>
       ) : (
         <>
-        <div>Please Log In</div>
-        <button onClick={() => signIn()}>Sign in </button>
+          <div>Please Log In</div>
+          <button onClick={() => signIn()}>Sign in </button>
         </>
       )}
     </div>
--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,11 +1,21 @@
 import LoginButton from "../../../components/LoginButton";
 import FavoriteSpot from "../../../components/FavoriteSpot";
+import styled from "styled-components";
 
+const ProfileContainer = styled.div`
+  overflow: auto;
+  height: 100;
+`;
+
+const Separator = styled.div`
+  border-block-end: 1px solid rgb(233, 232, 232);
+`;
 export default function ProfilePage() {
   return (
-    <div>
+    <ProfileContainer>
       <LoginButton />
-      <FavoriteSpot/> 
-    </div>
+      <Separator />
+      <FavoriteSpot />
+    </ProfileContainer>
   );
 }
