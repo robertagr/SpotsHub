d47841b984d683b027426c5b0c839fb917b1c71e
ROBERTA GRASSO <robertagrasso95@hotmail.com>
2023-08-14 16:55:30 +0200

    added error-isloading to favButton

--- components/FavoriteButton.js
+++ components/FavoriteButton.js
@@ -20,6 +20,8 @@ const HeartIcon = styled(({ isFavorite, ...props }) =>
 const FavoriteButton = ({ spotId, onFavoriteChange }) => {
   const { data: sessionData } = useSession();
   const [isFavorite, setIsFavorite] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   // Function to handle the click on the favorite button
   const handleFavoriteClick = async () => {
@@ -68,19 +70,30 @@ const FavoriteButton = ({ spotId, onFavoriteChange }) => {
       try {
         const res = await fetch(`/api/favorites/${sessionData.user._id}`);
         if (!res.ok) {
-          throw new Error("Error fetching favorite spots.");
+          throw new Error("Error fetching favorite status.");
         }
         const favoriteData = await res.json();
         const isSpotFavorite = favoriteData.some(
           (spot) => spot.spotId?._id === spotId
         );
         setIsFavorite(isSpotFavorite);
+        setIsLoading(false);
       } catch (error) {
         console.error("Error fetching favorite status:", error);
+        setError(error);
+        setIsLoading(false);
       }
     };
     fetchFavoriteStatus();
   }, [spotId, sessionData]);
+
+  if (isLoading) {
+    return null;
+  }
+
+  if (error) {
+    return <div>Error Fav Button: {error.message}</div>;
+  }
 
   return (
     <HeartButton onClick={handleFavoriteClick}>
